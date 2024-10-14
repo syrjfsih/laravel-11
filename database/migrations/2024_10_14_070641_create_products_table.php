9415b4ca5bff20e163cca19e047e35934b0ba1a0
syrjfsih <rajzchanel@gmail.com>
2024-10-14 14:59:48 +0700

    syrjfsih

---
+++ database/migrations/2024_10_14_070641_create_products_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('image');
+            $table->string('title');
+            $table->text('description');
+            $table->bigInteger('price');
+            $table->integer('stock')->default(0);
+            $table->timestamps();
+        });
+    }
+};
